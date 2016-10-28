6ec7c032b3a87159f14d6d2138e03d73fc8b57b0
Rob Cresswell <robert.cresswell@outlook.com>
2016-10-28 13:25:57 +0200

    Update xstatic-angular-gettext to 2.3.8.0
    Change-Id: Ic0c5929299b6f5ad9347b579b0f9d60d1921bfd9

--- xstatic/pkg/angular_gettext/__init__.py
+++ xstatic/pkg/angular_gettext/__init__.py
@@ -11,9 +11,9 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '2.1.0' # version of the packaged files, please use the upstream
+VERSION = '2.3.8' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '1' # our package build number, so we can release new builds
+BUILD = '0' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
@@ -24,19 +24,21 @@ CLASSIFIERS = []
 KEYWORDS = '%s xstatic' % NAME
 
 # XStatic-* package maintainer:
-MAINTAINER = 'Thai Tran'
-MAINTAINER_EMAIL = 'tqtran@us.ibm.com'
+MAINTAINER = 'Rob Cresswell'
+MAINTAINER_EMAIL = 'robert.cresswell@outlook.com'
 
 # this refers to the project homepage of the stuff we packaged:
 HOMEPAGE = 'https://angular-gettext.rocketeer.be/'
 
 # this refers to all files:
-LICENSE = '(same as %s)' % DISPLAY_NAME
+LICENSE = 'MIT'
 
 from os.path import join, dirname
 BASE_DIR = join(dirname(__file__), 'data')
 # linux package maintainers just can point to their file locations like this:
 #BASE_DIR = '/usr/share/javascript/jquery'
+
+MAIN='angular-gettext.js'
 
 LOCATIONS = {
     # CDN locations (if no public CDN exists, use an empty dict)
